6ce333f18441e3457107f4f30479a00d9f0bbaff
bangbang93 <bangbang93@163.com>
2021-06-05 10:32:57 +0800

    createLoggerService可以直接传options

--- src/logger.utils.ts
+++ src/logger.utils.ts
@@ -1,6 +1,11 @@
+import {createLogger, LoggerOptions} from 'bunyan'
 import {LoggerService} from './logger.service'
 import Logger = require('bunyan')
 
-export function createLoggerService(logger: Logger): LoggerService {
-  return new LoggerService(logger)
+export function createLoggerService(optionsOrLogger: LoggerOptions | Logger): LoggerService {
+  if (optionsOrLogger instanceof Logger) {
+    return new LoggerService(optionsOrLogger)
+  } else {
+    return new LoggerService(createLogger(optionsOrLogger))
+  }
 }
